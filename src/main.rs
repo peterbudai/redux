edbab1d74a4854578b672b69957ef70adb382e12
Péter Budai <buc@peterbudai.eu>
2015-04-13 23:01:57 +0200

    Started module implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io::*;
+
+mod redox;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = std::io::stdin();
+    let mut output  = std::io::stdout();
+    redox::compress(&mut input, &mut output);
 }
---
+++ src/redox.rs
@@ -0,0 +1,38 @@
+use std::io::Read;
+use std::io::Write;
+
+struct Status {
+    low: u64,
+    high: u64,
+    range: u64,
+    pending: u8,
+    freq: [u64; 258],
+    buf: [u8; 65536],
+    len: usize
+}
+
+impl Status {
+    fn init() -> Status {
+        let mut res = Status { 
+            low: 0x00000000_00000000u64, 
+            high: 0xFFFFFFFF_FFFFFFFFu64, 
+            range: 0u64, 
+            pending: 0u8, 
+            freq: [0u64; 258],
+            buf: [0u8; 65536],
+            len: 0us
+        };
+        for i in 1..258 {
+            res.freq[i] = i as u64;
+        }
+        return res;
+    }
+}
+
+pub fn compress(input: &mut Read, output: &mut Write) {
+    let st = Status::init();
+    for i in 0..258 {
+        println!("{} -> {}", i, st.freq[i]);
+    }
+}
+
