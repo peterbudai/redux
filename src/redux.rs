37f831ff0fa68fae61c1bf0d629a73b8a6185732
Péter Budai <buc@peterbudai.eu>
2015-04-17 23:11:31 +0200

    Fixed EOF handling

--- src/redux.rs
+++ src/redux.rs
@@ -1,5 +1,14 @@
 use std::io::Read;
 use std::io::Write;
+use std::io::Result;
+use std::io::Error;
+use std::io::ErrorKind;
+
+enum ReadResult<T> {
+    Ok(T),
+    Eof(T),
+    Err(Error)
+}
 
 struct Buffer {
     data: [u8; 256],
@@ -18,55 +27,68 @@ impl Buffer {
         };
     }
 
-    fn fill(&mut self, istream: &mut Read) -> bool {
+    fn fill(&mut self, istream: &mut Read) -> ReadResult<()> {
         match istream.read(&mut self.data[0..]) {
-            Ok(0) => false,
-            Ok(n) => { self.len = n; self.cur = 0; true },
-            Err(_) => false
+            Ok(0) => ReadResult::Eof(()),
+            Ok(n) => { self.len = n; self.cur = 0; ReadResult::Ok(()) },
+            Err(e) => ReadResult::Err(e)
         }
     }
 
-    fn pull(&mut self, value: &mut u8, istream: &mut Read) -> bool {
-        *value = (self.data[self.cur] & (1 << self.bit)) >> self.bit;
+    fn pull(&mut self, istream: &mut Read) -> ReadResult<u8> {
+        let value = (self.data[self.cur] & (1 << self.bit)) >> self.bit;
 
         if self.bit == 0 {
             self.bit = 7;
             self.cur += 1;
-            if self.cur == self.len && !self.fill(istream) {
-                return false;
+            if self.cur == self.len {
+                return match self.fill(istream) {
+                    ReadResult::Ok(_) => ReadResult::Ok(value),
+                    ReadResult::Eof(_) => ReadResult::Eof(value),
+                    ReadResult::Err(e) => ReadResult::Err(e)
+                }
             }
         } else {
             self.bit -= 1;
         }
-        return true;
+        return ReadResult::Ok(value);
     }
 
-    fn flush(&mut self, ostream: &mut Write) -> bool {
+    fn flush(&mut self, ostream: &mut Write) -> Result<()> {
         // Flush the remaining incomplete byte as well
         if self.bit < 7 {
             self.len += 1;
         }
 
-        match ostream.write(&self.data[..self.len]) {
-            Ok(n) => { self.len -= n; self.len == 0 },
-            _ => false
+        while self.cur < self.len {
+            match ostream.write(&self.data[self.cur..self.len]) {
+                Ok(0) => { return Err(Error::new(ErrorKind::WriteZero, "Zero bytes written to output")); }
+                Ok(n) => { self.cur += n; },
+                Err(e) => { return Err(e); }
+            }
         }
+
+        self.len = 0;
+        self.cur = 0;
+        return Ok(());
     }
 
-    fn push(&mut self, value: u8, ostream: &mut Write) -> bool {
+    fn push(&mut self, value: u8, ostream: &mut Write) -> Result<()> {
         self.data[self.len] |= value << self.bit;
 
         if self.bit == 0 {
             self.bit = 7;
             self.len += 1;
-            if self.len == self.data.len() && !self.flush(ostream) {
-                return false;
+            if self.len == self.data.len() {
+                if let Err(e) = self.flush(ostream) {
+                    return Err(e);
+                }
             }
             self.data[self.len] = 0;
         } else {
             self.bit -= 1;
         }
-        return true;
+        return Ok(());
     }
 }
 
@@ -102,13 +124,29 @@ pub fn compress(istream: &mut Read, ostream: &mut Write) {
     let mut st = Status::init();
     let mut v = 0u8;
     let mut count = 0u64;
-
-    st.input.fill(istream);
-    while st.input.pull(&mut v, istream) && st.output.push(v, ostream) {
-        count += 1;
+/*
+    if st.input.fill(istream) != IOResult::Success {
+        return;
     }
-    st.output.flush(ostream);
 
+    loop {
+        r = st.input.pull(&mut v, istream);
+
+        if st.output.push(v, ostream) == IOResult::Error { 
+            r = IOResult::Error;
+            break;
+        }
+
+        if r == IOResult::EOF {
+            break;
+        }
+
+        
+            count += 1;
+        }
+        st.output.flush(ostream);
+    }
+*/
     println!("{} bits", count);
 }
 
