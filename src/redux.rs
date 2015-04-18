8a2fb011bbbae236e9c14bf43b7b75d09a8d4797
Péter Budai <buc@peterbudai.eu>
2015-04-19 00:54:17 +0200

    Implemented compressor

--- src/redux.rs
+++ src/redux.rs
@@ -4,7 +4,14 @@ use std::io::Result;
 use std::io::Error;
 use std::io::ErrorKind;
 
-const BUFFER_SIZE : usize = 256us;
+const BUFFER_SIZE: usize = 256;
+const EOF_VALUE: usize = 256;
+const CODE_BITS: usize = 33;
+const CODE_MIN: u64 = 0;
+const CODE_MAX: u64 = (1 << CODE_BITS) - 1;
+const ONE_FOURTH: u64 = 1 << (CODE_BITS - 2);
+const ONE_HALF: u64 = 2 * ONE_FOURTH;
+const THREE_FOURTHS: u64 = 3 * ONE_FOURTH;
 
 struct Buffer {
     data: [u8; BUFFER_SIZE],
@@ -16,36 +23,49 @@ struct Buffer {
 
 impl Buffer {
     fn create() -> Buffer {
-        return Buffer { 
-            data: [0u8; BUFFER_SIZE], 
-            len: 0us, 
-            cur: 0us, 
+        return Buffer {
+            data: [0u8; BUFFER_SIZE],
+            len: 0us,
+            cur: 0us,
             bit: 7u8,
             sum: 0u64
         };
     }
 
-    fn fill(&mut self, istream: &mut Read) -> Result<bool> {
+    fn fill_buffer(&mut self, istream: &mut Read) -> Result<bool> {
         match istream.read(&mut self.data[0..]) {
             Ok(0) => Ok(true),
-            Ok(n) => { 
+            Ok(n) => {
                 self.sum += n as u64;
-                self.len = n; 
-                self.cur = 0; 
-                Ok(false) 
+                self.len = n;
+                self.cur = 0;
+                Ok(false)
             },
             Err(e) => Err(e)
         }
     }
 
-    fn pull(&mut self, istream: &mut Read) -> Result<(u8, bool)> {
+    fn get_byte(&mut self, istream: &mut Read) -> Result<(u8, bool)> {
+        let value = self.data[self.cur];
+        self.cur += 1;
+        if self.cur == self.len {
+            return match self.fill_buffer(istream) {
+                Ok(b) => Ok((value, b)),
+                Err(e) => Err(e)
+            }
+        } else {
+            return Ok((value, false));
+        }
+    }
+
+    fn get_bit(&mut self, istream: &mut Read) -> Result<(u8, bool)> {
         let value = (self.data[self.cur] & (1 << self.bit)) >> self.bit;
 
         if self.bit == 0 {
             self.bit = 7;
             self.cur += 1;
             if self.cur == self.len {
-                return match self.fill(istream) {
+                return match self.fill_buffer(istream) {
                     Ok(b) => Ok((value, b)),
                     Err(e) => Err(e)
                 }
@@ -56,7 +76,7 @@ impl Buffer {
         return Ok((value, false));
     }
 
-    fn flush(&mut self, ostream: &mut Write) -> Result<()> {
+    fn flush_buffer(&mut self, ostream: &mut Write) -> Result<()> {
         // Flush the remaining incomplete byte as well
         if self.bit < 7 {
             self.len += 1;
@@ -65,8 +85,8 @@ impl Buffer {
         while self.cur < self.len {
             match ostream.write(&self.data[self.cur..self.len]) {
                 Ok(0) => { return Err(Error::new(ErrorKind::WriteZero, "Zero bytes written to output")); }
-                Ok(n) => { 
-                    self.cur += n; 
+                Ok(n) => {
+                    self.cur += n;
                     self.sum += n as u64;
                 },
                 Err(e) => { return Err(e); }
@@ -78,14 +98,18 @@ impl Buffer {
         return Ok(());
     }
 
-    fn push(&mut self, value: u8, ostream: &mut Write) -> Result<()> {
+    fn put_byte(&mut self, value: u8, ostream: &mut Write) -> Result<()> {
+        return Ok(());
+    }
+
+    fn put_bit(&mut self, value: u8, ostream: &mut Write) -> Result<()> {
         self.data[self.len] |= value << self.bit;
 
         if self.bit == 0 {
             self.bit = 7;
             self.len += 1;
             if self.len == self.data.len() {
-                if let Err(e) = self.flush(ostream) {
+                if let Err(e) = self.flush_buffer(ostream) {
                     return Err(e);
                 }
             }
@@ -109,11 +133,11 @@ struct Status {
 
 impl Status {
     fn init() -> Status {
-        let mut res = Status { 
-            low: 0x00000000_00000000u64, 
-            high: 0xFFFFFFFF_FFFFFFFFu64, 
-            range: 0u64, 
-            pending: 0u8, 
+        let mut res = Status {
+            low: CODE_MIN,
+            high: CODE_MAX,
+            range: 0u64,
+            pending: 0u8,
             freq: [0u64; 258],
             input: Buffer::create(),
             output: Buffer::create()
@@ -124,32 +148,87 @@ impl Status {
         return res;
     }
 
+    fn get_probability(&self, chr: usize) -> (u64, u64, u64) {
+        (self.freq[chr], self.freq[chr + 1], self.freq[self.freq.len() - 1])
+    }
+
+    fn put_pending_bits(&mut self, bit: u8, ostream: &mut Write) -> Result<()> {
+        try!(self.output.put_bit(bit, ostream));
+        while self.pending > 0 {
+            try!(self.output.put_bit(bit, ostream));
+            self.pending -= 1;
+        }
+        return Ok(());
+    }
+
     fn metrics(&self) -> (u64, u64) {
-        return (self.input.sum , self.output.sum);
+        (self.input.sum , self.output.sum)
     }
 }
 
 pub fn compress(istream: &mut Read, ostream: &mut Write) -> Result<(u64, u64)> {
-    let mut st = Status::init();
-    let mut count = 0u64;
+    let mut status = Status::init();
 
-    if try!(st.input.fill(istream)) {
-        return Ok(st.metrics());
-    }
-
+    let mut eof = try!(status.input.fill_buffer(istream));
     loop {
-        let (v, eof) = try!(st.input.pull(istream));
-        try!(st.output.push(v, ostream));
+        let chr = if eof {
+            EOF_VALUE
+        } else {
+            match status.input.get_byte(istream) {
+                Ok((c, e)) => { eof = e; c as usize },
+                Err(e) => { return Err(e); }
+            }
+        };
 
-        count += 1;
-        if count >= 236 || eof {
+        {
+            let (low, high, count) = status.get_probability(chr);
+            status.range = status.high - status.low + 1;
+            status.high = status.low + (status.range * high / count) - 1;
+            status.low = low + (status.range * low / count);
+        }
+
+        loop {
+           if status.high < ONE_HALF {
+               try!(status.put_pending_bits(0, ostream));
+           } else if status.low >= ONE_HALF {
+               try!(status.put_pending_bits(1, ostream));
+           } else if status.low >= ONE_FOURTH && status.high < THREE_FOURTHS {
+                status.pending += 1;
+                status.low -= ONE_FOURTH;
+                status.high -= ONE_FOURTH;
+           } else {
+               break;
+           }
+
+           status.high = ((status.high << 1) + 1) & CODE_MAX;
+           status.low = (status.low << 1) & CODE_MAX;
+        }
+
+        if eof {
             break;
         }
     }
 
-    try!(st.output.flush(ostream));
+    status.pending += 1;
+    if status.low < ONE_FOURTH {
+        try!(status.put_pending_bits(0, ostream));
+    } else {
+        try!(status.put_pending_bits(1, ostream));
+    }
+    try!(status.output.flush_buffer(ostream));
 
-    println!("{} bits", count);
-    return Ok(st.metrics());
+    return Ok(status.metrics());
+}
+
+pub fn decompress(istream: &mut Read, ostream: &mut Write) -> Result<(u64, u64)> {
+    let mut status = Status::init();
+
+    let mut eof = try!(status.input.fill_buffer(istream));
+    loop {
+        break;
+    }
+    try!(status.output.flush_buffer(ostream));
+
+    return Ok(status.metrics());
 }
 
