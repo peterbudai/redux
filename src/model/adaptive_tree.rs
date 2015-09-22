99955d2555b66108b60667377e8d6bf4741c323e
Péter Budai <buc@peterbudai.eu>
2015-09-22 19:56:26 +0200

    Added tree manipulation code for encoding

--- src/model/adaptive_tree.rs
+++ src/model/adaptive_tree.rs
@@ -29,18 +29,40 @@ impl AdaptiveTreeModel {
     /// Initializes the model with the given parameters.
     pub fn new(p: Parameters) -> Box<AdaptiveTreeModel> {
         let mut m = AdaptiveTreeModel {
-            tree: vec![0; p.symbol_count + 1],
+            tree: vec![0; p.symbol_count],
             params: p,
         };
-        for i in 1..m.params.symbol_count {
+
+        m.tree[0] = 1u64;
+        for i in 1..m.tree.len() {
             m.tree[i] = i.last_one() as u64;
         }
 
         return Box::new(m);
     }
 
+    /// Returns the cumulated frequency of the symbol
+    fn get_frequency_single(&self, symbol: usize) -> u64 {
+        let mut i = symbol;
+        let mut sum = self.tree[0];
+        while i > 0 {
+            sum += self.tree[i];
+            i -= i.last_one();
+        }
+        return sum;
+    }
+
     /// Updates the cumulative frequencies for the given symbol.
     fn update(&mut self, symbol: usize) {
+        if symbol == 0 {
+            self.tree[0] += 1;
+        } else {
+            let mut i = symbol;
+            while i <= self.params.symbol_eof {
+                self.tree[i] += 1;
+                i += i.last_one();
+            }
+        }
     }
 }
 
@@ -50,21 +72,37 @@ impl Model for AdaptiveTreeModel {
     }
 
     fn total_frequency(&self) -> u64 {
-        0
+        self.get_frequency_single(self.params.symbol_eof)
     }
 
     fn get_frequency(&mut self, symbol: usize) -> Result<(u64, u64)> {
         if symbol > self.params.symbol_eof {
-            return Err(Error::InvalidInput)
+            return Err(Error::InvalidInput);
         }
 
-        let mut i = symbol;
-        let mut sum = 0u64;
-        while i > 0 {
-            sum += self.tree[i];
-            i -= i.last_one();
-        }
-        return Ok((sum, 0));
+        let result = if symbol > 0 {
+            let mut sumh = 0u64;
+            let mut suml = 0u64;
+            let mut h = symbol;
+            let mut l = symbol - 1;
+            while h != l {
+                if h > l {
+                    sumh += self.tree[h];
+                    h -= h.last_one();
+                } else {
+                    suml += self.tree[l];
+                    l -= l.last_one();
+                }
+            }
+
+            let sumr = self.get_frequency_single(h);
+            (suml + sumr, sumh + sumr)
+        } else {
+            (0u64, self.tree[0])
+        };
+
+        self.update(symbol);
+        return Ok(result);
     }
 
     fn get_symbol(&mut self, value: u64) -> Result<(usize, u64, u64)> {
