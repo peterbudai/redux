c1e95955dcec8c44fbca938058358b384d22e5a1
Péter Budai <buc@peterbudai.eu>
2016-02-14 13:23:22 +0100

    Rewrote tree model to use vanilla BIT algorithm, it works now without tweaks

--- src/model/adaptive_tree.rs
+++ src/model/adaptive_tree.rs
@@ -32,19 +32,18 @@ impl AdaptiveTreeModel {
     /// Initializes the model with the given parameters.
     pub fn new(p: Parameters) -> Box<AdaptiveTreeModel> {
         let mut m = AdaptiveTreeModel {
-            tree: vec![0; p.symbol_count],
+            tree: vec![0; p.symbol_count + 1],
             params: p,
         };
 
-        m.tree[0] = 1u64;
-        for i in 1..m.tree.len() {
+        for i in 0..m.tree.len() {
             m.tree[i] = i.last_one() as u64;
         }
 
         return Box::new(m);
     }
 
-    /// Returns the cumulated frequency of the symbol
+    /// Returns the cumulated frequency of the symbol.
     fn get_frequency_single(&self, symbol: usize) -> u64 {
         let mut i = symbol;
         let mut sum = self.tree[0];
@@ -55,39 +54,33 @@ impl AdaptiveTreeModel {
         return sum;
     }
 
-    fn get_frequency_inner(&mut self, symbol: usize) -> (u64, u64) {
-        if symbol > 0 {
-            let mut sumh = 0u64;
-            let mut suml = 0u64;
-            let mut h = symbol;
-            let mut l = symbol - 1;
-            while h != l {
-                if h > l {
-                    sumh += self.tree[h];
-                    h -= h.last_one();
-                } else {
-                    suml += self.tree[l];
-                    l -= l.last_one();
-                }
+    /// Returns cumulated frequency range of the symbol.
+    /// Uses an optimized algorithm to walk the common part of the tree only once.
+    fn get_frequency_range(&mut self, symbol: usize) -> (u64, u64) {
+        let mut sumh = 0u64;
+        let mut suml = 0u64;
+        let mut h = symbol + 1;
+        let mut l = symbol;
+        while h != l {
+            if h > l {
+                sumh += self.tree[h];
+                h -= h.last_one();
+            } else {
+                suml += self.tree[l];
+                l -= l.last_one();
             }
-
-            let sumr = self.get_frequency_single(h);
-            (suml + sumr, sumh + sumr)
-        } else {
-            (0u64, self.tree[0])
         }
+
+        let sumr = self.get_frequency_single(h);
+        (suml + sumr, sumh + sumr)
     }
 
     /// Updates the cumulative frequencies for the given symbol.
     fn update(&mut self, symbol: usize) {
-        if symbol == 0 {
-            self.tree[0] += 1;
-        } else {
-            let mut i = symbol;
-            while i <= self.params.symbol_eof {
-                self.tree[i] += 1;
-                i += i.last_one();
-            }
+        let mut i = symbol;
+        while i <= self.params.symbol_count {
+            self.tree[i] += 1;
+            i += i.last_one();
         }
     }
 }
@@ -98,15 +91,15 @@ impl Model for AdaptiveTreeModel {
     }
 
     fn total_frequency(&self) -> u64 {
-        self.get_frequency_single(self.params.symbol_eof)
+        self.get_frequency_single(self.params.symbol_count)
     }
 
     fn get_frequency(&mut self, symbol: usize) -> Result<(u64, u64)> {
         if symbol > self.params.symbol_eof {
             Err(Error::InvalidInput)
         } else {
-            let result = self.get_frequency_inner(symbol);
-            self.update(symbol);
+            let result = self.get_frequency_range(symbol);
+            self.update(symbol + 1);
             Ok(result)
         }
     }
@@ -118,39 +111,26 @@ impl Model for AdaptiveTreeModel {
         while (m > 0) && (i < self.params.symbol_eof) {
             let ti = i + m;
             let tv = self.tree[ti];
-
-            if v == tv {
-                break;
-            }
-            if v > tv {
+            if v >= tv {
                 i = ti;
                 v -= tv;
             }
             m >>= 1;
         }
 
-        let (mut l, mut h) = self.get_frequency_inner(i);
-
-        // TODO: Fixup logic, review algorithm and eventually remove this, as it slows operation
-        while value < l {
-            i -= 1;
-            h = l;
-            l = if i > 0 { self.get_frequency_single(i - 1) } else { 0u64 };
+        let (l, h) = self.get_frequency_range(i);
+        if value >= h {
+            Err(Error::InvalidInput)
+        } else {
+            self.update(i + 1);
+            Ok((i, l, h))
         }
-        while value >= h {
-            i += 1;
-            l = h;
-            h = self.get_frequency_single(i);
-        }
-
-        self.update(i);
-        Ok((i, l, h))
     }
 
     fn get_freq_table(&self) -> Vec<(u64, u64)> {
         let mut res = vec![(0u64, 0u64); self.params.symbol_count];
         for i in 0..self.params.symbol_count {
-            res[i] = (if i > 0 { self.get_frequency_single(i-1) } else { 0u64 }, self.get_frequency_single(i));
+            res[i] = (self.get_frequency_single(i), self.get_frequency_single(i + 1));
         }
         res
     }
--- tests/model.rs
+++ tests/model.rs
@@ -14,10 +14,18 @@ fn print_freq_tables(linear: &Vec<(u64, u64)>, tree: &Vec<(u64, u64)>) {
     }
 }
 
+fn get_invalid_symbol(bits: usize) -> usize {
+    (1usize << bits) + 1usize
+}
+
+fn get_valid_symbol(bits: usize) -> usize {
+    rand::random::<usize>() % get_invalid_symbol(bits)
+}
+
 #[test]
 fn compare_models_encode() {
-    let mut linear = AdaptiveLinearModel::new(Parameters::new(8, 14, 16).unwrap());
-    let mut tree = AdaptiveTreeModel::new(Parameters::new(8, 14, 16).unwrap());
+    let mut linear = AdaptiveLinearModel::new(Parameters::new(4, 14, 16).unwrap());
+    let mut tree = AdaptiveTreeModel::new(Parameters::new(4, 14, 16).unwrap());
 
     println!("Encode test");
     for count in 0..10000 {
@@ -26,21 +34,26 @@ fn compare_models_encode() {
         assert_eq!(lt, tt);
         let lf = linear.get_freq_table();
         let tf = tree.get_freq_table();
-        // print_freq_tables(&lf, &tf);
+        print_freq_tables(&lf, &tf);
         assert_eq!(lf, tf);
-        let symbol = rand::random::<u8>() as usize;
+        let symbol = get_valid_symbol(4); 
         let (ll, lh) = linear.get_frequency(symbol).unwrap();
         let (tl, th) = tree.get_frequency(symbol).unwrap();
         println!("Iteration: {}, Symbol: {}, Linear: {}-{}, Tree: {}-{}", count, symbol, ll, lh, tl, th);
         assert_eq!(ll, tl);
         assert_eq!(lh, th);
     }
+
+    assert!(!linear.get_frequency(get_invalid_symbol(4)).is_ok());
+    assert!(!linear.get_frequency(get_invalid_symbol(4) + 1).is_ok());
+    assert!(!tree.get_frequency(get_invalid_symbol(4)).is_ok());
+    assert!(!tree.get_frequency(get_invalid_symbol(4) + 1).is_ok());
 }
 
 #[test]
 fn compare_models_decode() {
-    let mut linear = AdaptiveLinearModel::new(Parameters::new(8, 14, 16).unwrap());
-    let mut tree = AdaptiveTreeModel::new(Parameters::new(8, 14, 16).unwrap());
+    let mut linear = AdaptiveLinearModel::new(Parameters::new(4, 14, 16).unwrap());
+    let mut tree = AdaptiveTreeModel::new(Parameters::new(4, 14, 16).unwrap());
 
     println!("Decode test");
     for count in 0..10000 {
@@ -49,9 +62,9 @@ fn compare_models_decode() {
         assert_eq!(lt, tt);
         let lf = linear.get_freq_table();
         let tf = tree.get_freq_table();
-        // print_freq_tables(&lf, &tf);
+        print_freq_tables(&lf, &tf);
         assert_eq!(lf, tf);
-        let value = (rand::random::<u64>() % lt) as u64;
+        let value = rand::random::<u64>() % lt;
         let (ls, ll, lh) = linear.get_symbol(value).unwrap();
         let (ts, tl, th) = tree.get_symbol(value).unwrap();
         println!("Iteration: {}, Value: {}, Linear: {}, {}-{}, Tree: {}, {}-{}", count, value, ls, ll, lh, ts, tl, th);
@@ -59,5 +72,13 @@ fn compare_models_decode() {
         assert_eq!(ll, tl);
         assert_eq!(lh, th);
     }
+
+    let lf = linear.total_frequency();
+    assert!(!linear.get_symbol(lf).is_ok());
+    assert!(!linear.get_symbol(lf + 1).is_ok());
+
+    let tf = tree.total_frequency();
+    assert!(!tree.get_symbol(tf).is_ok());
+    assert!(!tree.get_symbol(tf + 1).is_ok());
 }
 
